1bc8018648e5a7953b3f1325d5442b7f8e9bf761
Ethan Wang <acitrusfruit42@gmail.com>
2013-08-24 19:48:25 -0400

    show who started a poll

--- tour.js
+++ tour.js
@@ -1095,7 +1095,7 @@ var cmds = {
 		var answers = answers.join(',').toLowerCase().split(',');
 		tour[room.id].question = question;
 		tour[room.id].answerList = answers;
-		room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
+		room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION<br /><font color = "black"><i>Poll started by ' + user.name + '</i></font></small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
 	},
 
 	vote: function(target, room, user) {
