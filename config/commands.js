bab48096c7f9ee3046ee7dbb003888c402e27ca8
jd4564 <thelittlejd09@gmail.com>
2013-09-04 01:12:15 -0300

    Add the Gym Leader group to /groups

--- config/commands.js
+++ config/commands.js
@@ -636,11 +636,12 @@ var commands = exports.commands = {
 
 	groups: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />' +
-			'% <b>Driver</b> - The above, and they can also mute and lock users and check for alts<br />' +
-			'@ <b>Moderator</b> - The above, and they can ban users<br />' +
-			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties<br />'+
-			'~ <b>Administrator</b> - They can do anything, like change what this message says');
+		this.sendReplyBox('± <b>Gym Leader</b> - These are the Gym Leaders of the server. <br />' +
+			'+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat.<br />' +
+			'% <b>Driver</b> - The above, and they can also mute and lock users and check for alts.<br />' +
+			'@ <b>Moderator</b> - The above, and they can ban users.<br />' +
+			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties.<br />'+
+			'~ <b>Administrator</b> - They can do anything, like change what this message says.');
 	},
 
 	opensource: function(target, room, user) {
