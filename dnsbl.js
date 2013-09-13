b02b5a3b84ca42c4fe5aafd3a462b9c22c521634
jd4564 <thelittlejd09@gmail.com>
2013-09-13 16:19:28 -0300

    Don't block IP's if they are listed in xbl.spamhaus.org.
    xbl.spamhaus.org is intended for use only on incoming mail, it contains
    mostly dynamic IP addresses.
    Using it for anything other than it's intended purpose may block
    legitimate users.

--- dnsbl.js
+++ dnsbl.js
@@ -3,7 +3,6 @@ var dns = require('dns');
 var blocklist = 'zen.spamhaus.org';
 
 var dnsblCache = {};
-
 exports.query = function queryDnsbl(ip, callback) {
 	if (ip in dnsblCache) {
 		callback(dnsblCache[ip]);
@@ -11,6 +10,7 @@ exports.query = function queryDnsbl(ip, callback) {
 	}
 	var reversedIp = ip.split('.').reverse().join('.');
 	dns.resolve4(reversedIp+'.'+blocklist, function(err, addresses) {
+		if (!err && blocklist == "zen.spamhaus.org" && addresses[0] == "127.0.0.4") return; 
 		var isBlocked = dnsblCache[ip] = !err;
 		callback(isBlocked);
 	});
