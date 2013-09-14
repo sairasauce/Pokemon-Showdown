b44eef5ae3f951836f374448f71298187ce47527
jd4564 <thelittlejd09@gmail.com>
2013-09-13 23:47:46 -0300

    Remove my outdated dnsbl system

--- config/config.js
+++ config/config.js
@@ -125,7 +125,6 @@ exports.inactiveuserthreshold = 1000*60*60;
 // Set this to true if you are using Pokemon Showdown on Heroku.
 exports.herokuhack = false;
 
-exports.blacklist = true;
 // Custom avatars.
 // This allows you to specify custom avatar images for users on your server.
 // Place custom avatar files under the /config/avatars/ directory.
