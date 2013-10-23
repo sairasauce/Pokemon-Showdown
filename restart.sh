198d8ea3f0a29b77b005f528319157a69fcb893d
jd4564 <thelittlejd09@gmail.com>
2013-10-22 23:17:38 -0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if  pgrep node > /dev/null
 then
-	pkill node && screen -dmS pokemonshowdown node app.js 
+	pkill node && screen -L -dmS pokemonshowdown node app.js &
 else 
-	screen -dmS pokemonshowdown node app.js 
+	screen -L -dmS pokemonshowdown node app.js &
 fi
-
