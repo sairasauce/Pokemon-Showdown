70aa190473ab485d448a558eafd978eb1bc77225
Ethan Wang <acitrusfruit42@gmail.com>
2013-11-09 23:57:32 -0500

    update section for ou monotype

--- config/formats.js
+++ config/formats.js
@@ -134,7 +134,7 @@ exports.Formats = [
 	},
 	{
 		name: "OU Monotype",
-		section: "Other Metagames",
+		section: "XY Singles",
 
 		ruleset: ['Pokemon', 'Standard Pokebank', 'Same Type Clause', 'Evasion Abilities Clause', 'Team Preview'],
 		banlist: ['Uber', 'Soul Dew']
